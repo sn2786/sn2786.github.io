66bec0c4e263d9f5b777098d708759ad0d6addb2
foo@bar.com <sn2786>
2019-04-12 18:59:38 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -8,11 +8,13 @@ const HOSTED_URLS = {
 
 const examples = {
   'example1':
-      'Alice was beginning to get very tired of sitting by her sister on the bank.',
+      "Two other passengers, besides the one, were plodding up the hill by the side of the mail.",
   'example2':
-      'Buda-Pesth seems a wonderful place.',
+      "Buda-Pesth seems a wonderful place.",
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
+      "Scepticism was as much the result of knowledge, as knowledge is of scepticism.",
+  'example4':
+      "The craving for reasons is instinctive, and the dreamer's sense of logic even dominates his sense of time."
 };
 
 function status(statusText) {
@@ -47,11 +49,11 @@ function doPredict(predict) {
   const result = predict(textField.value);
   score_string = "Class scores: ";
   for (var x in result.score) {
-    score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
+    score_string += x + " ->  " + result.score[x].toFixed(30) + ", "
   }
   //console.log(score_string);
   status(
-      score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
+      score_string + ' elapsed: ' + result.elapsed.toFixed(30) + ' ms)');
 }
 
 function prepUI(predict) {
